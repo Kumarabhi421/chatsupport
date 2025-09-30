7ec2458055bd3fd6fa95c834fce42e79a6235caa
Kumarabhi421 <159116112+Kumarabhi421@users.noreply.github.com>
2025-09-30 16:09:05 +0530

    Update serializers.py

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,15 +1,40 @@
 
 
-from rest_framework import serializers
-from .models import Message, ContactInfo
+# from rest_framework import serializers
+# from .models import Message, ContactInfo
 
+# class MessageSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Message
+#         fields = ['id', 'sender', 'text', 'timestamp', 'contact']
+
+# class ContactInfoSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ContactInfo
+#         fields = ['id', 'token_number', 'contact_type', 'contact_value', 'ip_address',
+#                   'mobile', 'email', 'created_at', 'is_seen']
+
+
+
+
+from rest_framework import serializers
+from .models import Message, ContactInfo, WebsiteRegistration
+# ----------------- Messages -----------------
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'text', 'timestamp', 'contact']
+        fields = ['id', 'sender', 'text', 'timestamp', 'chat']
 
+# ----------------- Contact Info -----------------
 class ContactInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactInfo
-        fields = ['id', 'token_number', 'contact_type', 'contact_value', 'ip_address',
-                  'mobile', 'email', 'created_at', 'is_seen']
+        fields = ['id', 'token_number', 'contact_type', 'contact_value', 
+                  'ip_address', 'mobile', 'email', 'created_at', 'is_seen']
+
+# ----------------- Website Registration -----------------
+class WebsiteRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WebsiteRegistration
+        fields = '__all__'
+
