e8118247e9ef5b173062b7fb170aea0453f3a29e
Kumarabhi421 <sabhishekkumar514@gmail.com>
2025-10-07 16:32:32 +0530

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -732,9 +732,10 @@ def extract_contact_info(text: str):
     return mobile, email
 
 # ================= FRONT VIEWS =================
-# def chat_view(request):
-#     website_id = "ocCzClIRkv"  # ya dynamic logic se pick karo
-#     return render(request, "chat.html", {"website_id": website_id})
+def chat_view(request):
+    website_id = request.GET.get('website_id', 'local')
+    return render(request, "chat.html", {"website_id": website_id})
+
 
 # def chat_view(request):
 #     host = request.get_host()  # example: "example.com:8000"
@@ -745,20 +746,20 @@ def extract_contact_info(text: str):
 #         website_id = None  # fallback, ya show error
 #     return render(request, "chat.html", {"website_id": website_id})
 
-def chat_view(request):
-    website_id = request.GET.get('website_id')
-    if not website_id:
-        return render(request, "error.html", {"message": "Website ID missing"})
+# def chat_view(request):
+#     website_id = request.GET.get('website_id')
+#     if not website_id:
+#         return render(request, "error.html", {"message": "Website ID missing"})
     
-    # Database में check करो कि website_id valid है या नहीं
-    from .models import WebsiteRegistration
-    try:
-        website = WebsiteRegistration.objects.get(website_id=website_id)
-    except WebsiteRegistration.DoesNotExist:
-        return render(request, "error.html", {"message": "Invalid Website ID"})
+#     # Database में check करो कि website_id valid है या नहीं
+#     from .models import WebsiteRegistration
+#     try:
+#         website = WebsiteRegistration.objects.get(website_id=website_id)
+#     except WebsiteRegistration.DoesNotExist:
+#         return render(request, "error.html", {"message": "Invalid Website ID"})
     
-    # अगर valid है, तो chat widget page render करो
-    return render(request, "chat.html", {"website": website})
+#     # अगर valid है, तो chat widget page render करो
+#     return render(request, "chat.html", {"website": website})
 
 # ================= WEBSITE ADMIN REGISTER =================
 def website_admin_register(request):
